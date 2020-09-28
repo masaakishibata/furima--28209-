6f67f82c004e15cc5f0b9c235af592291dded3d0
masaaki-shibata <69470093+masaaki-shibata@users.noreply.github.com>
2020-09-28 16:08:42 +0900

    テーブル設計に向けたカラム整理

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_180714) do
+ActiveRecord::Schema.define(version: 2020_09_28_070710) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -22,11 +22,11 @@ ActiveRecord::Schema.define(version: 2020_09_26_180714) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "last_name"
-    t.string "family_name"
-    t.string "password"
-    t.string "birthday"
     t.string "first_name"
+    t.string "first_name_kana"
+    t.string "family_name"
+    t.string "family_name_kana"
+    t.string "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
