59a7c3deefa7398913ee82d35377aa895599d577
masaaki-shibata <69470093+masaaki-shibata@users.noreply.github.com>
2020-10-02 23:22:01 +0900

    エラー対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :user
-  post 'sessions#create'
+  # post 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
