f4d892ae30771d95f187a7261f4576c6dd87f806
masaaki-shibata <69470093+masaaki-shibata@users.noreply.github.com>
2020-10-05 19:09:19 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
   validates :email, uniqueness: true
   validates :password, presence: true, length: {minimum: 6}
 end
