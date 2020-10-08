96fa9cdb7e3ffee7401f83143af0372719e38086
masaaki-shibata <69470093+masaaki-shibata@users.noreply.github.com>
2020-10-08 14:12:22 +0900

    レビュー修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
-    validates :first_name
-    validates :last_name
-  end
+  with_options presence: true do
+    validates :birthday
+    validates :nickname
+    validates :email, uniqueness: true
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width characters' } do
-    validates :first_name_kana
-    validates :last_name_kana
-  end
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+      validates :first_name
+      validates :last_name
+    end
 
-  with_options presence: true, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'Password reading is invalid. Input full-width characters' } do
-    validates :password
-  end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width characters' } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
 
-  validates :birthday, presence: true
-  validates :nickname, presence: true
-  validates :email, uniqueness: true
+    with_options format: { with: /\A[a-zA-Z0-9]+\z/, message: 'Password reading is invalid. Input full-width characters' } do
+      validates :password, length: {minimum: 6} 
+    end
+  end
 end
