c5135a52e3f82d03c82dba88206a24cffdf64771
masaaki-shibata <69470093+masaaki-shibata@users.noreply.github.com>
2020-10-20 18:58:46 +0900

    修正３

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -20,7 +20,6 @@ const pay = () => {
         const renderDom = document.getElementById('charge-form');
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-        debugger;
       };
 
       document.getElementById("card-number").removeAttribute("name");
