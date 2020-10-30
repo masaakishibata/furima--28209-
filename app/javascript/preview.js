99f186d4fac422e8d2454cb06d77a633208cc6c0
masaaki-shibata <69470093+masaaki-shibata@users.noreply.github.com>
2020-10-30 13:21:08 +0900

    複数のpreview機能実装、フロント

---
+++ app/javascript/preview.js
@@ -0,0 +1,40 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ItemImages = document.getElementById('images-list');
+    console.log(ItemImages)
+    
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div');
+      imageElement.setAttribute('class', 'image-element');
+      let imageElementNum = document.querySelectorAll('.image-element').length
+
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      blobImage.setAttribute('class', 'image-element-img-src');
+
+      const inputHTML = document.createElement('input');
+      inputHTML.setAttribute('id', `item_image_${imageElementNum}`);
+      inputHTML.setAttribute('name', 'item[images][]');
+      inputHTML.setAttribute('type', 'file');
+
+      imageElement.appendChild(blobImage);
+      imageElement.appendChild(inputHTML);
+      ItemImages.appendChild(imageElement);
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+        
+        createImageHTML(blob)
+      })
+    }
+
+    document.getElementById('item-image').addEventListener('change', (e) => {
+      let file = e.target.files[0];
+      let blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob)
+
+    });
+  });
+}
