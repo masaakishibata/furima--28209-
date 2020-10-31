2f251ffedaec7b22fa6960b18d6bac81a8debdeb
masaaki-shibata <69470093+masaaki-shibata@users.noreply.github.com>
2020-10-31 22:08:34 +0900

    修正１

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ApplicationRecord
   has_many :item_tag_relations
   has_many :tweets, through: :item_tag_relations
 
-  validates :name, uniqueness: true
+  validates :tagname, uniqueness: true
 end
