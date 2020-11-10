d5d4883ee63b761f8dc4df8867ca7917dbbd6249
masaaki-shibata <69470093+masaaki-shibata@users.noreply.github.com>
2020-11-10 15:58:01 +0900

    コメント機能実装：完了

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
-
+  before_action :add_comment, only: [new, :create]
   def new
     @comments = Comment.all
     @comment = Comment.new
   end
 
   def create
-    @comment = Comment.new(text: params[:comment] [:text])
+    @comment = Comment.new(text: params[:comment] [:text] )
     if @comment.save
       ActionCable.server.broadcast 'comment_channel', content: @comment
     end
@@ -17,9 +17,13 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.requier(:comment).permit(:text).merge(
+    params.require(:comment).permit(:text).merge(
       user_id: current_user.id,
       item_id: params[:item_id]
     )
+  end
 
+  def add_comment
+    @add_comment = comment_params
+  end
 end
